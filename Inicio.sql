e8259e3efa5a33e8c2310ead6f98a2f339614090
Fingulfin Isildorf <146995145+fingulfin@users.noreply.github.com>
2024-10-14 08:04:44 -0600

    Add files via upload

--- Inicio.sql
+++ Inicio.sql
@@ -1,1 +1,15 @@
-show databases
+-- Para comentar usamos doble guion
+/*  
+ Comentarios de multiples lineas usamos digonal asterisco y cerramos con
+ asterico diagonal
+*/
+-- Mostrar las bases de datos disponibles:
+show databases;
+
+-- Usar una base de datos
+use u375466133_c5Hvp;
+
+-- Mostrar las tablas de la base de datos
+show tables;
+
+
