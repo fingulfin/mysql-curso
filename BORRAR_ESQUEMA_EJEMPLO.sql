c7285f6778bcace391f96632f59744c506778f3a
Fingulfin Isildorf <146995145+fingulfin@users.noreply.github.com>
2024-10-23 07:43:44 -0600

    Add files via upload

---
+++ BORRAR_ESQUEMA_EJEMPLO.sql
@@ -0,0 +1,38 @@
+
+
+drop table if exists order_details;
+drop table if exists orders;
+drop table if exists items;
+
+
+drop table if exists customers;
+drop table if exists engine_sample;
+drop table if exists sales_totals;
+drop table if exists sales_reps;
+drop table if exists paid_invoices;
+drop table if exists active_invoices;
+drop table if exists date_sample;
+drop table if exists float_sample;
+drop table if exists string_sample;
+drop table if exists color_sample;
+drop table if exists customers;
+drop table if exists projects;
+drop table if exists employees;
+drop table if exists departments;
+drop table if exists null_sample;
+drop table if exists invoice_archive;
+drop table if exists vendor_contacts;
+drop index invoices_invoice_date_ix on invoices;
+drop table if exists invoice_line_items;
+drop table if exists invoices;
+drop table if exists vendors;
+drop table if exists terms;
+drop table if exists general_ledger_accounts;
+drop table if exists customers;
+
+
+drop table if exists order_details;
+drop table if exists orders;
+drop table if exists items;
+
+drop table if exists customers2;
